a5683555e19a85dddb3ef191b75be636efbc1a56
muhammadkashif20 <kashif28@gmail.com>
2024-11-03 09:26:11 +0500

    Work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Header />
       <Services/>
       <Footer />
+    
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/Header",
-    element:<Header/>
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
